d21e6ef88c8d4a08683c7c21158430897dec0555
renju.ashokan <renjuashokan@gmail.com>
2021-01-19 15:21:23 +0530

    Adding project TestGTKUI

---
+++ TestGTKUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Gtk;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace TestGTKUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var assemblyName = new AssemblyName("TestGTKUI");
+            var resources = string.Join(Environment.NewLine, Assembly.Load(assemblyName).GetManifestResourceNames());
+            Console.WriteLine("List of Manifest Resource Names");
+            Console.WriteLine("======================");
+            Console.WriteLine(resources);
+            GladeApp(args);
+        }
+        public static void GladeApp(string[] args)
+        {
+            Application.Init();
+            var cssProvider = new CssProvider();
+            cssProvider.LoadFromResource("TestGTKUI.cssfiles.main.css");
+            //cssProvider.LoadFromPath("main.css");
+            Gtk.StyleContext.AddProviderForScreen(Gdk.Screen.Default, cssProvider, Gtk.StyleProviderPriority.User);
+
+            Console.WriteLine("Creating window now");
+            testWindow twindow = testWindow.Create();
+            twindow.ShowAll();
+            Application.Run();
+        }
+    }
+}
---
+++ TestGTKUI/testWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gtk;
+
+namespace TestGTKUI
+{
+    public class testWindow : Window
+    {
+        private Builder builder;
+        public static testWindow Create()
+        {
+            Builder _builder = new Builder(null, "TestGTKUI.test.glade", null);
+            return new testWindow(_builder, _builder.GetObject("testWindow").Handle);
+        }
+        protected testWindow(Builder _builder, IntPtr handle) : base(handle)
+        {
+            builder = _builder;
+            builder.Autoconnect(this);
+            Console.WriteLine("testWindow is created");
+        }
+
+        private void on_buttonClickme_clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button clicked");
+        }
+    }
+}
