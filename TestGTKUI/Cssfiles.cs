5943e840dbded80c46f07a38693b82690d093779
renju.ashokan <renjuashokan@gmail.com>
2021-04-22 11:13:21 +0530

    Updating to 3.24.24.34

---
+++ TestGTKUI/Cssfiles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGTKUI
+{
+    public class CSSData
+    {
+        public string cssdata = @"
+        GtkButton,
+        .button_style {
+            color: black;
+            font-weight: bold;
+        }
+
+        GtkLabel,
+        .labelStyle {
+            color: forestgreen;
+            font-weight: bold;
+            font-size: 15px;
+        }
+        ";
+
+        public CSSData()
+        {
+            Console.WriteLine("ctr");
+        }
+
+        public string CSSString
+        {
+            get => cssdata;
+        }
+    }
+    
+}
